24dce2e9a6850d85ce1b51e7621795b1d2254b58
casolanki <uvchsolanki@gmail.com>
2021-11-07 22:20:40 +0800

    structre component and toastr service

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from './../_services/account.service';
 import { Observable } from 'rxjs';
 import { User } from '../_model/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -12,7 +14,8 @@ import { User } from '../_model/user';
 export class NavComponent implements OnInit {
   model: any = {} 
 
-  constructor(public accountService: AccountService) {
+  constructor(public accountService: AccountService,
+              private router: Router,private toastr: ToastrService) {
   }
 
   ngOnInit() {   
@@ -20,14 +23,16 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
